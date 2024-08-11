c9ad11da2263db9432afb52bcec15035cc168da8
Luis Agreda <lmagreda-sanchez@myseneca.ca>
2024-08-11 15:44:54 -0400

    chore: Update file paths for movie and ratings datasets

--- app/models/model.py
+++ app/models/model.py
@@ -11,8 +11,8 @@ import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-movies = pd.read_parquet("../data/movies.parquet")
-ratings = pd.read_parquet("../data/ratings.parquet")
+movies = pd.read_parquet("/data/movies.parquet")
+ratings = pd.read_parquet("/data/ratings.parquet")
 
 
 def clean_title(title):
