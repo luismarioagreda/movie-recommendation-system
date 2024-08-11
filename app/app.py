6a7b718ca7e1d1b49c864c52d674619ef88f78e0
Luis Agreda <lmagreda-sanchez@myseneca.ca>
2024-08-11 15:26:48 -0400

    optimized for deployment

--- app/app.py
+++ app/app.py
@@ -1,13 +1,8 @@
-import os
-from io import BytesIO
-
-import dotenv
-import requests
 import streamlit as st
 from PIL import Image
+from models.model import find_similar_movies, search, visualize_movie_ratings
+import pandas as pd
 
-dotenv.load_dotenv()
-API_URL = os.getenv("API_URL")
 
 st.title(":red[Notflix]")
 
@@ -20,39 +15,28 @@ if "selected_movie" not in st.session_state:
 if "search_results" not in st.session_state:
     st.session_state.search_results = []
 
-# Send request to Flask API when button clicked
 if st.button("Search"):
-    response = requests.post(f"{API_URL}/search", json={"title": user_input})
-    st.session_state.search_results = response.json()
+    response = search(user_input)
+    st.session_state.search_results = response
 
 # Display search results
-if st.session_state.search_results:
+if len(st.session_state.search_results) > 0:
     st.text("Search Results:")
-    for result in st.session_state.search_results:
+    for _, result in st.session_state.search_results.iterrows():
         if st.button(result["title"], key=result["movieId"]):
             st.session_state.selected_movie = result
 
 # Display the selected movie
-if st.session_state.selected_movie:
-    selected_movie = st.session_state.selected_movie
+if st.session_state.selected_movie is not None and not st.session_state.selected_movie.empty:
+    selected_movie = st.session_state.selected_movie.to_dict()
     st.write(f"Selected Movie: {selected_movie['title']}")
-
+    
     # Display the rating histogram
-    response = requests.post(
-        f"{API_URL}/visualize",
-        json={"movieId": selected_movie["movieId"]},
-    )
-    img_data = response.content
-    img = Image.open(BytesIO(img_data))
+    img_data = visualize_movie_ratings(selected_movie["movieId"])
+    img = Image.open(img_data)
     st.image(img, caption="Rating Distribution", use_column_width=True)
-
+    
     # Display recommendations for the selected movie
-    response = requests.post(
-        f"{API_URL}/recommendations",
-        json={"movieId": selected_movie["movieId"]},
-    )
-    results = response.json()
-
+    results = find_similar_movies(selected_movie["movieId"])
     st.header("You should watch...")
-    for result in results:
-        st.markdown(f'- {result["title"]}')
+    st.dataframe(results[["title", "genres"]], hide_index=True )
